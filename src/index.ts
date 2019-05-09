1bc817e12e55c122a2e11a3c0534e913026ff8f2
Felix WATTEZ <felixwattez@gmail.com>
2019-05-09 15:19:03 +0200

    Server port in env variable

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import * as bodyParser from "body-parser";
 import Server from "./server";
 import router from "./router";
 
-const server = Server.init(8080);
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+
+const server = Server.init(port);
 
 server.app.use(bodyParser.json());
 server.app.use(router);
